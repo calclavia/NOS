801a6a0a8c3d4aa623124c5289ec42a25b20f636
Henry Mao <admin@calclavia.com>
2019-06-07 20:02:00 -0700

    Added stack head as feature

--- meta_env/meta_env.py
+++ meta_env/meta_env.py
@@ -18,8 +18,8 @@ class MetaEnv(Env):
         self.dataset = dataset
         self.batch_size = batch_size
         self.cuda = cuda
+        self.observation_space = spaces.Box(low=-100, high=100, shape=(len(all_ops) + 1,), dtype=np.float32)
         self.action_space = spaces.Discrete(len(all_ops))
-        self.observation_space = spaces.Discrete(len(all_ops))
         self.train_loader, self.val_loader = create_datasets(dataset, batch_size, cuda)
         self.model = ConvModel()
 
@@ -28,12 +28,16 @@ class MetaEnv(Env):
         self.init_state = self.model.state_dict()
 
     def reset(self):
+        self.max_train_iters = 100
         self.executor = StackMachine()
         # Initialize dummy variable
         self.executor.init()
         self.executor.reset(torch.tensor(0.0), torch.tensor(1.0))
         self.instrs = []
-        return 0
+
+        action_features = [0 for _ in range(len(all_ops))]
+        state = np.array(action_features + self.get_stack_features())
+        return state
 
     def get_valid_actions(self):
         """ Get a vector of valid actions """
@@ -45,59 +49,75 @@ class MetaEnv(Env):
         assert self.action_space.contains(action)
         
         done = False
-        state = action
         reward = 0
         self.instrs.append(action)
+        is_invalid = torch.isnan(self.executor.stack[-1]).any().item() or torch.isinf(self.executor.stack[-1]).any().item()
 
-        if torch.isnan(self.executor.stack[-1]).any().item():
+        if is_invalid:
             # Early terminate if nan
             done = True
             reward -= 1
+            res = 0
             print('Early terminate nan')
-        elif self.executor(action) is not None:
-            ops = [all_ops[x] for x in self.instrs]
-            done = True
-            train_failed = False      
-            model = deepcopy(self.model)
+        else:
+            res = self.executor(action)
+            if res is not None:
+                res = res.item()
 
-            optimizer = MetaOptimizer(model.parameters(), self.instrs)
+                ops = [all_ops[x] for x in self.instrs]
+                done = True
+                train_failed = False      
+                model = deepcopy(self.model)
 
-            num_epochs = 1
-            # total_iters = num_epochs * len(self.train_loader)
-            for e in range(num_epochs):
-                model.train()
-                # Train on training set
-                iterations = 0
-                for x, y in self.train_loader:
-                    if self.cuda:
-                        x, y = x.cuda(), y.cuda()
+                optimizer = MetaOptimizer(model.parameters(), self.instrs)
 
-                    y_pred = model(x)
-                    loss = F.cross_entropy(y_pred, y)
-                    model.zero_grad()
-
-                    if torch.isnan(loss).any().item():
-                        train_failed = True
-                        print('Failed train', iterations, loss.item(), ops)
-                        break
-                    
-                    iterations += 1
-                    loss.backward()
-                    optimizer.step()
-
-                if train_failed:
-                    break
-
-            if not train_failed:
-                # Validation set
-                with torch.no_grad():
-                    model.eval()
-                    accs = []
-                    for x, y in self.val_loader:
+                num_epochs = 1
+                # total_iters = num_epochs * len(self.train_loader)
+                for e in range(num_epochs):
+                    model.train()
+                    # Train on training set
+                    iterations = 0
+                    for x, y in self.train_loader:
                         if self.cuda:
                             x, y = x.cuda(), y.cuda()
+
                         y_pred = model(x)
-                        accs += (y_pred.argmax(dim=-1) == y).tolist()
-                reward += np.mean(accs)
-                print('Validation Acc:', np.mean(accs), ops)
+                        loss = F.cross_entropy(y_pred, y)
+                        model.zero_grad()
+
+                        if torch.isnan(loss).any().item():
+                            train_failed = True
+                            print('Failed train', iterations, loss.item(), ops)
+                            break
+                        
+                        iterations += 1
+                        loss.backward()
+                        optimizer.step()
+                        if iterations > self.max_train_iters:
+                            break
+
+                    if train_failed:
+                        break
+
+                if not train_failed:
+                    # Validation set
+                    with torch.no_grad():
+                        model.eval()
+                        accs = []
+                        for x, y in self.val_loader:
+                            if self.cuda:
+                                x, y = x.cuda(), y.cuda()
+                            y_pred = model(x)
+                            accs += (y_pred.argmax(dim=-1) == y).tolist()
+                    reward += np.mean(accs)
+                    print('Validation Acc:', np.mean(accs), ops)
+
+        # Features
+        action_features = [a == action for a in range(len(all_ops))]
+        state = np.array(action_features + self.get_stack_features(res))
         return state, reward, done, {}
+
+    def get_stack_features(self, res=0):
+        bound = 100
+        stack_features = [min(max(self.executor.stack[-1], -bound), bound) if len(self.executor.stack) > 0 else res]
+        return stack_features if np.isfinite(stack_features) else [0 for _ in stack_features]
